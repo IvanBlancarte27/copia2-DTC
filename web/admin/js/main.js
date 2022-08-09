5993ec76f2f7e778d3bcee84ab70bfc19dcc1adb
MissaFirstX <araizamartinezmissael@gmail.com>
2022-08-09 11:57:46 -0500

    version2

--- web/admin/js/main.js
+++ web/admin/js/main.js
@@ -17,14 +17,14 @@ function cargarModuloEmpleado()
         //dentro del contenedor principal
         document.getElementById('contenedor_principal').innerHTML=datos;
                  
-        import('./empleado.js') 
-                .then(obj => {
+        import('./empleado.js').then(obj => {
                     cm=obj;
                     cm.inicializar();
                     
                 });
              });  
 }
+
 function cargarModuloCliente()
 {
      //AJAX: Asynchronous
@@ -48,6 +48,7 @@ function cargarModuloCliente()
                 });
              });   
 }
+
 function cargarModuloAccesorio()
 {
     //AJAX: Asynchronous
